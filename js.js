76d5452803967be5e870fcdb689bbd90799b3a68
Monika <codingbymon@gmail.com>
2024-04-02 12:15:13 +0200

    downloading elements into js

---
+++ js.js
@@ -0,0 +1,7 @@
+const playBtn = document.querySelector('.player__button')
+const progressRate = document.querySelector('.progress__filled')
+const volumeRate = document.querySelector('input[name="volume"]')
+const playbackRate = document.querySelector('input[name="playbackRate"]')
+const minusBtn = document.querySelector('button[data-skip="-10"]')
+const plusBtn = document.querySelector('button[data-skip="25"]')
+
