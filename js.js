dcd315c4b897bcd30edde17ddd5fe923d02d097f
Monika <codingbymon@gmail.com>
2024-04-02 13:56:47 +0200

    writing the function updateButton

--- js.js
+++ js.js
@@ -1,7 +1,33 @@
-const playBtn = document.querySelector('.player__button')
-const progressRate = document.querySelector('.progress__filled')
-const volumeRate = document.querySelector('input[name="volume"]')
-const playbackRate = document.querySelector('input[name="playbackRate"]')
-const minusBtn = document.querySelector('button[data-skip="-10"]')
-const plusBtn = document.querySelector('button[data-skip="25"]')
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const toggleBtn = document.querySelector('.toggle')
+const progress = document.querySelector('.progress')
+const progressBar = document.querySelector('.progress__filled')
+const skipBtns = document.querySelectorAll('[data-skip]')
+const ranges = document.querySelectorAll('.player__slider')
 
+function togglePlay() {
+	if (video.paused) {
+		video.play()
+	} else {
+		video.pause()
+	}
+	// inny zapis
+	// const method = video.paused ? 'play' : 'pause'
+	// video[method]()
+}
+
+function updateButton() {
+	console.log(this)
+	const icon = this.paused ? '▶' : '⏸'
+	toggleBtn.textContent = icon
+}
+
+
+
+toggleBtn.addEventListener('click', togglePlay)
+video.addEventListener('click', togglePlay)
+
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+// te listenery są nadane na akcję play i pause, a nie tylko na klik, ponieważ użytkownik może zastopować czy włączyć play w różny sposób, na przykład, może używać wtyczki, która po zminimalizowaniu karty stopuje film - wtedy również chcemy aby nasza funkcja się wykonała
