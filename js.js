4433b7f3904233d369d48a36ae1f0ceb3cd96940
Monika <codingbymon@gmail.com>
2024-04-02 15:59:42 +0200

    finish task

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ const progress = document.querySelector('.progress')
 const progressBar = document.querySelector('.progress__filled')
 const skipBtns = document.querySelectorAll('[data-skip]')
 const ranges = document.querySelectorAll('.player__slider')
+const fullScreenBtn = document.querySelector('.full__screen')
 
 function togglePlay() {
 	if (video.paused) {
@@ -29,10 +30,29 @@ function skip() {
 }
 
 function handleRangeUpdate() {
-	console.log(this.value)
-	console.log(this.name)
-    video[this.name] = this.value
-// Nawiasy kwadratowe ([]) są używane w JavaScript do dostępu do właściwości obiektu za pomocą zmiennej jako klucza. W przypadku linii kodu video[this.name] = this.value, używane są nawiasy kwadratowe, ponieważ this.name jest zmienną, która przechowuje nazwę właściwości, do której chcemy się odwołać w obiekcie video. Dla przykładu, jeśli this.name zawierałoby wartość "volume", linia kodu video[this.name] = this.value byłaby równoważna video["volume"] = this.value, co oznaczałoby przypisanie wartości this.value do właściwości volume obiektu video.
+	video[this.name] = this.value
+	// Nawiasy kwadratowe ([]) są używane w JavaScript do dostępu do właściwości obiektu za pomocą zmiennej jako klucza. W przypadku linii kodu video[this.name] = this.value, używane są nawiasy kwadratowe, ponieważ this.name jest zmienną, która przechowuje nazwę właściwości, do której chcemy się odwołać w obiekcie video. Dla przykładu, jeśli this.name zawierałoby wartość "volume", linia kodu video[this.name] = this.value byłaby równoważna video["volume"] = this.value, co oznaczałoby przypisanie wartości this.value do właściwości volume obiektu video.
+}
+
+function handleProgress() {
+	// flex-basis - właściwość ustawiona w css
+
+	const percent = (video.currentTime / video.duration) * 100
+	progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+	console.log(e)
+}
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        video.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
 }
 
 toggleBtn.addEventListener('click', togglePlay)
@@ -52,3 +72,13 @@ ranges.forEach(range => {
 ranges.forEach(range => {
 	range.addEventListener('mousemove', handleRangeUpdate)
 })
+video.addEventListener('timeupdate', handleProgress)
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousedown', () => mousedown = true)
+progress.addEventListener('mouseup', () => mousedown = false)
+
+fullScreenBtn.addEventListener('click', toggleFullScreen)
