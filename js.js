8356daeec6a877d4f0320b31321705ed76c4c100
Monika <codingbymon@gmail.com>
2024-04-02 14:42:46 +0200

    update application

--- js.js
+++ js.js
@@ -23,7 +23,17 @@ function updateButton() {
 	toggleBtn.textContent = icon
 }
 
+function skip() {
+	console.log(this.dataset.skip)
+	video.currentTime += parseInt(this.dataset.skip)
+}
 
+function handleRangeUpdate() {
+	console.log(this.value)
+	console.log(this.name)
+    video[this.name] = this.value
+// Nawiasy kwadratowe ([]) są używane w JavaScript do dostępu do właściwości obiektu za pomocą zmiennej jako klucza. W przypadku linii kodu video[this.name] = this.value, używane są nawiasy kwadratowe, ponieważ this.name jest zmienną, która przechowuje nazwę właściwości, do której chcemy się odwołać w obiekcie video. Dla przykładu, jeśli this.name zawierałoby wartość "volume", linia kodu video[this.name] = this.value byłaby równoważna video["volume"] = this.value, co oznaczałoby przypisanie wartości this.value do właściwości volume obiektu video.
+}
 
 toggleBtn.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
@@ -31,3 +41,14 @@ video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 // te listenery są nadane na akcję play i pause, a nie tylko na klik, ponieważ użytkownik może zastopować czy włączyć play w różny sposób, na przykład, może używać wtyczki, która po zminimalizowaniu karty stopuje film - wtedy również chcemy aby nasza funkcja się wykonała
+
+skipBtns.forEach(btn => {
+	btn.addEventListener('click', skip)
+})
+
+ranges.forEach(range => {
+	range.addEventListener('change', handleRangeUpdate)
+})
+ranges.forEach(range => {
+	range.addEventListener('mousemove', handleRangeUpdate)
+})
